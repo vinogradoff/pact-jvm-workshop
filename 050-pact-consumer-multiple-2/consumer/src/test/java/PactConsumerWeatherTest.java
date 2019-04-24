58a75486dad5c5399d5a6ff66e99772d477c0837
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2019-04-25 01:17:10 +0200

    added lab 050

---
+++ 050-pact-consumer-multiple-2/consumer/src/test/java/PactConsumerWeatherTest.java
@@ -0,0 +1,95 @@
+import au.com.dius.pact.consumer.*;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.model.RequestResponsePact;
+import de.vinogradoff.pact101.consumer.rest.Controller;
+import de.vinogradoff.pact101.consumer.rest.Weather;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactFolder("build/pact-files")
+public class PactConsumerWeatherTest {
+
+  Map<String, String> headers = MapUtils.putAll(new HashMap<String, String>(),
+          new String[]{"Content-Type", "application/json"});
+
+  @Pact(provider="weather_service", consumer="news_portal")
+  public RequestResponsePact createPact(PactDslWithProvider builder) {
+
+    return builder
+            .uponReceiving("Request the weather conditions for city")
+            .path("/weather/now")
+            .method("GET")
+            .matchQuery("city", ".*", "Moscow")
+            .willRespondWith()
+            .headers(headers)
+            .status(200)
+            .body( "{\"city\":\"Berlin\"," +
+                    "\"temperature\":23.4}"
+            )
+            .toPact();
+  }
+
+  @Pact(provider="weather_service", consumer="news_portal")
+  public RequestResponsePact createPactPart2(PactDslWithProvider builder) {
+
+    return builder
+            .uponReceiving("Request the weather conditions for city second interaction")
+            .path("/weather/now")
+            .method("GET")
+            .matchQuery("city", ".*", "Berlin")
+            .willRespondWith()
+            .headers(headers)
+            .status(200)
+            .body( "{\"city\":\"Berlin\"," +
+                    "\"temperature\":23.4}"
+            )
+            .toPact();
+  }
+
+
+
+  /* this just creates a pact file */
+  //@Test
+  void runMock(MockServer mockServer) throws IOException {
+    HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/weather/now?city=London").execute().returnResponse();
+    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+  }
+
+
+  @Test
+  @PactTestFor(pactMethod = "createPact")
+  void unitTestCustomerMethod(MockServer mockServer) {
+    Controller ctrl=new Controller();
+    ctrl.serviceUrl=mockServer.getUrl()+"/weather/now";
+    Weather weather=  ctrl.weatherNow("Moscow2");
+    assertThat(weather.getCity()).isEqualTo("Berlin");
+    assertThat(weather.getTemperature()).isGreaterThan(0);
+    assertThat(weather.getHot()).isFalse();
+
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "createPactPart2")
+  void unitTestCustomerMethod2(MockServer mockServer) {
+    Controller ctrl=new Controller();
+    ctrl.serviceUrl=mockServer.getUrl()+"/weather/now";
+    Weather weather=  ctrl.weatherNow("Moscow2");
+    assertThat(weather.getCity()).isEqualTo("Berlin");
+    assertThat(weather.getTemperature()).isGreaterThan(0);
+    assertThat(weather.getHot()).isFalse();
+
+  }
+
+}
